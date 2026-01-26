7858e3f17f87b1196dd4f802b4373ad030954fcf
Kanasagara Het Sanjaybhai <hetkanasagara7@gmail.com>
2026-01-26 10:42:33 +0530

    Time: 299 ms (56.24%), Space: 40.9 MB (15.73%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        pair = [(p, s) for p, s in zip(position, speed)]
+        stack = []
+
+        for p, s in sorted(pair)[::-1]:
+            time = float(target - p) / s   # 🔥 FIX
+            stack.append(time)
+
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+        return len(stack)

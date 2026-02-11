cd4c548afc9c1c79b05078d63001f9f2a60f87ee
Kanasagara Het Sanjaybhai <hetkanasagara7@gmail.com>
2026-02-11 19:01:37 +0530

    Time: 0 ms (100%), Space: 7.7 MB (83.16%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.c
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -1,10 +1,23 @@
+#include <stdbool.h>
+
 bool isPerfectSquare(int num) {
-    int i = 1;
+    if (num < 1) return false;
     
-    while (num > 0) {
-        num -= i;
-        i += 2;
+    long left = 1, right = num;
+    
+    while (left <= right) {
+        long mid = left + (right - left) / 2;
+        
+        if (mid == num / mid && num % mid == 0) {
+            return true; 
+        }
+        else if (mid > num / mid) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
     }
     
-    return num == 0;
+    return false;
 }
